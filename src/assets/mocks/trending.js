e566ba8085fcda8aff1e9e45962853678b277aff
Miron Banks <madapegarage@gmail.com>
2020-04-29 16:11:19 +0200

    assets for mocks

---
+++ src/assets/mocks/trending.js
@@ -0,0 +1,32 @@
+import moment from 'moment'
+
+export default [
+    {
+        title: 'Software Engineer Salary in 2020',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'money.jpg',
+    },
+    {
+        title: 'GraphQL vs REST',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['React', 'JavaScript'],
+        link: '#',
+        image: 'apollo_grqphql.jpg',
+    },
+    {
+        title: 'A Day in Life of a Programmer',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'programmer.jpg',
+    },
+    {
+        title: 'Brain Hack for Learning to Program',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Brain Health'],
+        link: '#',
+        image: 'neuron.jpg',
+    },
+]
